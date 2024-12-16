a198e67ae7697ee8500b848b58817e1383403371
Ubuntu <ubuntu@ip-172-31-27-246.eu-north-1.compute.internal>
2024-12-16 13:44:11 +0000

    Visual Design

--- tutorindigo/templates/indigo/lms/static/js/dark-theme.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    'use strict';
-
-    const enableDarkTheme = {% if INDIGO_ENABLE_DARK_THEME %}true{% else %}false{% endif %};
-    if (enableDarkTheme){
-        $('body').addClass("indigo-dark-theme");
-    }
-});
